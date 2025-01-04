e47645ad69ff947912ba12d30baedf1d2256a33b
AymanAnwar <tajallaarshad734@gmail.com>
2025-01-03 23:51:12 -0800

    done rejex

---
+++ revision.js
@@ -0,0 +1,72 @@
+//  var age = 24;
+//  console.log (age);
+
+//  var name = "Ahmad";
+//  console.log (name);
+
+//  var fruits = ["apple", "mango", "banana"];
+//  console.log (fruits); 
+
+//  console.log (fruits[2]);
+
+
+
+// var name = "Ayman Anwar";
+// var age = 20;
+// var isStudent = true ;
+// console.log(name);
+// console.log(age);
+// console.log(isStudent);
+
+// var x = 10;
+// console.log(x);
+//     x = 20;
+// console.log(x);
+
+// const pi = 3.142;
+// console.log(pi);
+
+// pi = 2.132;
+// console.log(pi);
+
+// var myVar;
+// console.log(myVar);
+// console.log(typeof(myVar));
+//     myVar="Ayman";
+//     console.log(myVar);
+//     console.log(typeof(myVar));
+//     myVar=25;
+//     console.log(myVar);
+//     console.log(typeof(myVar));
+//     myVar=true;
+//     console.log(myVar);
+//     console.log(typeof(myVar));
+
+//     var a = 24;
+//     console.log(a);
+//         console.log(typeof(a));
+//         a = "";
+//         console.log(a);
+//         console.log(typeof(a));
+//         a = 23;
+//         console.log(a);
+//         console.log(typeof(a));
+//         a = true;
+//         console.log(a);
+//         console.log(typeof(a));
+//         a = "";
+//         console.log(a);
+//         console.log(typeof(a));
+//         a = false;
+//         console.log(a);
+//         console.log(typeof(a));
+
+
+  var age = 20;
+if (age>18){
+    console.log("you're allowed");
+}
+else {
+    console.log("you're not allowed")
+}
+elseif (age<50)
